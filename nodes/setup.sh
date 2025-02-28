255a31006f9d846ce6f326ee0b06c971b5925a78
GenAI Works <admin@genai.works>
2025-02-28 21:23:19 +0200

    Update setup.sh

--- nodes/setup.sh
+++ nodes/setup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo "SERVER_ID = $SERVER_ID"
-echo "HOSTNAME = $HOSTNAME"
-
 # Replace placeholders in the my.cnf file with actual environment variable values
 sed -i "s/@@SERVER_ID@@/$SERVER_ID/g" /etc/mysql/my.cnf
 sed -i "s/@@HOSTNAME@@/$HOSTNAME/g" /etc/mysql/my.cnf
