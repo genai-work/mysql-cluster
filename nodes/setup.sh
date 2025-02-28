6090e1c24ad5167f824f12d8c3b9d27bb340ec37
GenAI Works <admin@genai.works>
2025-02-28 21:16:38 +0200

    Update setup.sh

--- nodes/setup.sh
+++ nodes/setup.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+set -e
 
-# Replace placeholders in the my.cnf file with actual environment variable values
+# Проверяем, что переменные окружения установлены
+: "${SERVER_ID:?Переменная SERVER_ID не установлена}"
+: "${HOSTNAME:?Переменная HOSTNAME не установлена}"
+
+# Заменяем плейсхолдеры в файле конфигурации my.cnf
 sed -i "s/@@SERVER_ID@@/$SERVER_ID/g" /etc/mysql/my.cnf
 sed -i "s/@@HOSTNAME@@/$HOSTNAME/g" /etc/mysql/my.cnf
 
-# Start MySQL server
-docker-entrypoint.sh mysqld
+# Выводим итоговую конфигурацию для проверки
+echo "Конфигурация my.cnf после замены:"
+grep -E "server_id|report_host" /etc/mysql/my.cnf
+
+# Запускаем MySQL сервер, используя exec для корректного перенаправления сигналов
+exec docker-entrypoint.sh mysqld
